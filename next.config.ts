9b2176e0b0ce8e4af37acb480414d2d85b0907f6
Samdav2 <dawodujohn1@gmail.com>
2026-03-23 08:15:30 +0100

    fixing build error

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,10 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'app.echelontix.com.ng',
+      },
+      {
+        protocol: 'https',
+        hostname: 'echelon-backend.onrender.com/',
       }
     ],
   },
